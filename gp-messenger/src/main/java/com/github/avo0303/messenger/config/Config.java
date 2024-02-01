4eee693e9fff6120594982c54d881e6f20826c1c
Andrew Ovchinnikov <andrewdecker07@gmail.com>
2024-02-01 14:24:47 +0300

    feat: add config to handle props

---
+++ gp-messenger/src/main/java/com/github/avo0303/messenger/config/Config.java
@@ -0,0 +1,27 @@
+package com.github.avo0303.messenger.config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+  private final Properties properties;
+
+  public Config() {
+    properties = new Properties();
+    try (InputStream input = getClass().getClassLoader().getResourceAsStream(
+        "application.properties")) {
+      if (input == null) {
+        System.out.println("Sorry, unable to find config.properties");
+        return;
+      }
+      properties.load(input);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+}
